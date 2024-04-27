f7ee51f409168d4f6cb2417d93379be316656b91
Kouhei.Sano <apple.x.co@gmail.com>
2024-04-27 13:38:44 +0900

    chore:

--- source/app/var/qiq/template/Error.php
+++ source/app/var/qiq/template/Error.php
@@ -1,1 +1,7 @@
-ERROR
+<?php
+assert($this instanceof \Qiq\Template);
+$this->setLayout('layout/base');
+?>
+<h2>Exception</h2>
+<p>{{h $e['code'] }}</p>
+<p>{{h $e['class'] }} ({{h $e['message'] }})</p>
